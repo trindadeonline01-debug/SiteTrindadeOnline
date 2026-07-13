77a1018c27af9f0aaa044751409e951f10b54082
trindadeonline01-debug <trindadeonline01@gmail.com>
2026-07-13 21:40:36 +0000

    fix: atualizar VAPID key Firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
 
-export const VAPID_KEY = 'P7GXNTWLKJ9WW-yiFm-VCdMn2afjY1WYB2PFMDAaf1Q'
+export const VAPID_KEY = 'BGjXQZSfLUILJ43yf4B_Q3sQlpoPXsO8CXZsQrlaxJrowQUGvl_PeCWztUEnCPJpF4Cy3BBAbfydLF8J1uQPxY0'
 
 export function getFirebaseMessaging() {
   if (typeof window === 'undefined') return null

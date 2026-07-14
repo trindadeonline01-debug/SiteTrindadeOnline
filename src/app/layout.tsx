04a2ce0324b77c6878db115392140d50cdc36c8b
trindadeonline01-debug <trindadeonline01@gmail.com>
2026-07-14 14:54:16 +0000

    feat: Google Tag Manager instalado no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,10 +27,34 @@ export const metadata: Metadata = {
   alternates: { canonical: 'https://trindadeonline.com.br' },
 }
 
+const GTM_ID = 'GTM-P3889L2G'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body><TopNav />{children}<BottomNav /><Script src='https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js' strategy='afterInteractive'/></body>
+      <head>
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`
+          }}
+        />
+      </head>
+      <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{display:'none',visibility:'hidden'}}
+          />
+        </noscript>
+        <TopNav />
+        {children}
+        <BottomNav />
+        <Script src='https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js' strategy='afterInteractive'/>
+      </body>
     </html>
   )
 }
